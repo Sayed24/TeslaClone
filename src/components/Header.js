23a69b777574474909f7375baa2f683169b42d57
Sayed Sadat <SadatSayed92@gmail.com>
2021-06-09 17:12:04 -0700

    Sidebar Menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import MenuIcon from '@material-ui/icons/Menu';
+import CloseIcon from '@material-ui/icons/Close';
 
 function Header() {
     return (
@@ -20,6 +21,20 @@ function Header() {
                     <MenuIcon />
                 </MenuIconContainer>
             </RightMenu>
+            <BurgerNav>
+                <CloseContainer>
+                    <Close />
+                </CloseContainer>
+                <li><a href="#">Model S</a></li>
+                <li><a href="#">Model Y</a></li>
+                <li><a href="#">Existing Inventory</a></li>
+                <li><a href="#">Used Inventory</a></li>
+                <li><a href="#">Trade-in</a></li>
+                <li><a href="#">Cybertruck</a></li>
+                <li><a href="#">Roadster</a></li>
+                <li><a href="#">Semi</a></li>
+                <li><a href="#">Charging</a></li>
+            </BurgerNav>
         </Container>
     )
 }
@@ -32,7 +47,12 @@ const Container = styled.div`
     min-height: 60px;
     align-items: center;
     justify-content: space-between;
-    padding: 0 20px
+    padding: 0 20px;
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    z-index: 10;
 `
 
 const MenuGroup = styled.div`
@@ -59,5 +79,35 @@ const MenuIconContainer = styled.div`
     display: flex;
     align-items: center;
     cursor: pointer;
+
+`
+
+const CloseContainer = styled.div`
+    display: flex;
+    justify-content: flex-end;
+`
+
+const Close = styled(CloseIcon)`
+    cursor: pointer;
+
+`
+
+const BurgerNav = styled.div`
+    position: fixed;
+    width: 300px;
+    background: white;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    padding: 20px;
+    list-style: none;
+    li {
+        padding: 15px 0;
+        border-bottom: 1px solid rgba(0, 0, 0, .2);
+        a {
+            font-weight: 600;
+        }
+    }
+    
 
 `
